3ed9f8544580b6545b3da7350c7a708ad6846906
nxt <nxtnguyenxuantruongxt@gmail.com>
2025-04-25 13:06:05 +0700

    feat: integrate NotificationsService in CommentsService and LikesService for comment and like notifications

--- backend/src/comments/comments.module.ts
+++ backend/src/comments/comments.module.ts
@@ -6,9 +6,13 @@ import { User } from 'src/users/user.entity';
 import { Like } from 'src/likes/likes.entity';
 import { Post } from 'src/posts/posts.entity';
 import { CommentsResolver } from './comments.resolver';
+import { NotificationsModule } from '../notifications/notifications.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, Like, Post, Comment])],
+  imports: [
+    TypeOrmModule.forFeature([User, Like, Post, Comment]),
+    NotificationsModule,
+  ],
   providers: [CommentsService, CommentsResolver],
   exports: [CommentsService],
 })
